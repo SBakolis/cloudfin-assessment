a1b64f4d7ae991ff28a0c6946223cb486a77a53e
SBakolis <s.bakolis@outlook.com>
2024-07-05 22:33:55 +0300

    added angular material to facelift ui and implemented company update functionality

--- cloudfin-front/CompanyApp/src/app/company/company.component.ts
+++ cloudfin-front/CompanyApp/src/app/company/company.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
 import { CompanyService, Company } from '../company.service';
 
@@ -9,11 +14,22 @@ import { CompanyService, Company } from '../company.service';
   templateUrl: './company.component.html',
   styleUrls: ['./company.component.scss'],
   standalone: true,
-  imports: [CommonModule, FormsModule, HttpClientModule], // Import necessary modules
+  imports: [
+    CommonModule,
+    FormsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatInputModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatIconModule,
+  ],
 })
 export class CompanyComponent {
   companies: Company[] = [];
   newCompany: Company = { code: '', address: '', description: '' };
+  displayedColumns: string[] = ['code', 'address', 'description', 'actions'];
+  isEditMode = false;
 
   constructor(private companyService: CompanyService) {
     this.loadCompanies();
@@ -25,16 +41,33 @@ export class CompanyComponent {
     });
   }
 
-  addCompany(): void {
-    this.companyService.addCompany(this.newCompany).subscribe(() => {
-      this.loadCompanies();
-      this.newCompany = { code: '', address: '', description: '' };
-    });
+  editCompany(company: Company): void {
+    this.newCompany = { ...company };
+    this.isEditMode = true;
+  }
+
+  addOrUpdateCompany(): void {
+    if (this.isEditMode) {
+      this.companyService.updateCompany(this.newCompany).subscribe(() => {
+        this.loadCompanies();
+        this.resetForm();
+      });
+    } else {
+      this.companyService.addCompany(this.newCompany).subscribe(() => {
+        this.loadCompanies();
+        this.resetForm();
+      });
+    }
   }
 
   deleteCompany(code: string): void {
     this.companyService.deleteCompany(code).subscribe(() => {
       this.loadCompanies();
     });
+  }
+
+  resetForm(): void {
+    this.newCompany = { code: '', address: '', description: '' };
+    this.isEditMode = false;
   }
 }
--- cloudfin-front/CompanyApp/src/main.ts
+++ cloudfin-front/CompanyApp/src/main.ts
@@ -2,7 +2,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
 import { AppComponent } from './app/app.component';
 import { CompanyService } from './app/company.service';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideHttpClient(), CompanyService],
+  providers: [provideHttpClient(), CompanyService, provideAnimationsAsync()],
 }).catch((err) => console.error(err));
